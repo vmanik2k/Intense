7423c237da2e4981bc7ad2c82650dd78cf6306d0
Vishwajeet Behera <vishwajeetbehera0@gmail.com>
2021-11-19 12:40:34 +0530

    changes trial

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Intense Time',
+      title: 'Intense',
       debugShowCheckedModeBanner: false,
       home: StudentDashboard(),
     );
